f00e2dc18b0f40c36853412a9badd64bfb41f305
Abhimanyu Dasarwar <63962258+Abhimanyu9539@users.noreply.github.com>
2023-12-24 23:26:35 +0530

    Add files via upload

---
+++ Flask Wine Classification/app.py
@@ -0,0 +1,13 @@
+#importing 
+from flask import Flask
+#create an instance of Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
